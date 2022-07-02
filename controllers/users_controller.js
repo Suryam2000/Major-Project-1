b1b63792629291c76c3859ce122d028a11b205fe
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-07-03 01:48:25 +0530

    Added home and users.ejs in views and rendered page accordingly through controllers

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,7 @@
 module.exports.users = function(req, res){
-    return res.end('<h1>User Profile!!</h1>')
+    return res.render('users', {
+       users: 'User Profile' 
+    });
 };
 
 module.exports.posts = function(req, res){
